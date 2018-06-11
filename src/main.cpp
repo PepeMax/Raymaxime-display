14e129289d84f0feac319076a37ebf62422da01c
PépéMax <xdpepemaxdx@gmail.com>
2018-06-11 16:21:58 +0200

    Organization of the code for the WiFi

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,7 @@ const int   watchdog = 5000;        // Fréquence du watchdog - Watchdog frequen
 unsigned long previousMillis = millis();
 
 void setup() {
-
   Serial.begin(115200);
-  Serial.print("Connection to ");
-  Serial.println(WIFI_SSID);
-
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi Connected");
-  Serial.println("IP Adress: ");
-  Serial.println(WiFi.localIP());
 }
 
 void loop() {
@@ -28,5 +14,9 @@ void loop() {
 
   if (!isWifiConnected()) {
     ConnecteToWiFi(WIFI_SSID, WIFI_PASSWORD);
+    Serial.println("");
+    Serial.println("WiFi Connected");
+    Serial.println("IP Adress: ");
+    Serial.println(WiFi.localIP());
   }
 }
